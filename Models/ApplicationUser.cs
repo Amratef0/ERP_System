472934e4c4516a36a14fcb02aa6a8018e6b2a83f
Amratef0 <Amr45409@gmail.com>
2025-09-10 15:37:29 +0300

    Add Models

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ERP_System_Project.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+       public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public string? Address;
+    }
+}
