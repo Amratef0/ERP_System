8b1e2d1373eb08953ec067473bbb26a9aac4431e
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-09-13 15:12:42 +0300

    added pagination

---
+++ Helpers/Pagination.cs
@@ -0,0 +1,32 @@
+﻿using ERP_System_Project.DTOs;
+using ERP_System_Project.Repository.Interfaces;
+
+namespace ERP_System_Project.Helpers
+{
+    public static class Pagination<TEntity> where TEntity : class
+    {
+        public static async Task<PageSourcePagination<TEntity>> GetPaginatedData(
+            IRepository<TEntity> repository,
+            int pageNumber = 1, int pageSize = 10
+            )
+        {
+            if(pageNumber < 1) pageNumber = 1;
+            if(pageSize < 10) pageSize = 10;
+
+            var result = await repository
+                .SkipAndTake((pageNumber - 1 ) * pageSize , pageSize);
+
+            var totalRecords = await repository.Count();
+            var totalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+
+            return new PageSourcePagination<TEntity>
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalRecords = totalRecords,
+                TotalPages =  totalPages,
+                Data = result
+            };
+        }
+    }
+}
