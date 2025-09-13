24b2abc2b942f16db46ccf23eaee60d00822f2c7
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-09-13 16:31:02 +0300

    enhance Repository and make it flexable

--- Repository/Implementations/Repository.cs
+++ Repository/Implementations/Repository.cs
@@ -1,6 +1,9 @@
-﻿using ERP_System_Project.Models;
+﻿using ERP_System_Project.DTOs;
+using ERP_System_Project.Models;
 using ERP_System_Project.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace ERP_System_Project.Repository.Implementations
 {
@@ -13,19 +16,89 @@ namespace ERP_System_Project.Repository.Implementations
             _db = db;
             _dbSet = db.Set<T>();
         }
-        public async Task AddAsync(T entity)
-            => await _dbSet.AddAsync(entity);
 
-
+        #region Retriving
         public async Task<List<T>> GetAllAsync()
             => await _dbSet.ToListAsync();
 
         public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
 
+        public IQueryable<T> GetAllToIQueryable() => _dbSet;
 
-        public async Task<bool> IsExistAsync(int id)
-            => await _dbSet.FindAsync(id) == null ? false : true;
 
+        public Task<T?> GetAsync(Expression<Func<T, bool>> filter, params Expression<Func<T, object>>[] Includes)
+        {
+            IQueryable<T> query = _dbSet;
+
+            query = query.Where(filter);
+
+            foreach (var include in Includes)
+                query = query.Include(include);
+
+            return query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            params Expression<Func<T, object>>[] Includes)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (Includes != null)
+                foreach (var include in Includes)
+                    query = query.Include(include);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            return await query.ToListAsync();
+        }
+
+
+        public async Task<PageSourcePagination<T>> GetAllPaginatedAsync(
+            int pageNumber = 1, int pageSize = 10,
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            params Expression<Func<T, object>>[] Includes)
+        {
+            if (pageNumber < 1) pageNumber = 1;
+            if (pageSize < 10) pageSize = 10;
+
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            if (Includes != null)
+                foreach (var include in Includes)
+                    query = query.Include(include);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+
+            var result = _dbSet.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+
+            var totalRecords = await _dbSet.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+
+            return new PageSourcePagination<T>
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalRecords = totalRecords,
+                TotalPages = totalPages,
+                Data = await result.ToListAsync()
+            };
+        }
+        #endregion
+
+        #region Add_Update_Delete
+        public async Task AddAsync(T entity)
+            => await _dbSet.AddAsync(entity);
         public void Update(T entity)
             => _dbSet.Update(entity);
 
@@ -35,11 +108,16 @@ namespace ERP_System_Project.Repository.Implementations
             if (entity != null)
                 _dbSet.Remove(entity);
         }
+        #endregion
 
-        public async Task<List<T>> SkipAndTake(int skip, int take)
-            => await _dbSet.Skip(skip).Take(take).ToListAsync();
-
+        #region Otheres
+        public async Task<bool> IsExistAsync(int id)
+            => await _dbSet.FindAsync(id) == null ? false : true;
         public async Task<int> Count()
             => await _dbSet.CountAsync();
+        #endregion
+
+
+        
     }
 }
--- Repository/Interfaces/IRepository.cs
+++ Repository/Interfaces/IRepository.cs
@@ -1,19 +1,46 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using ERP_System_Project.DTOs;
+using System.Linq.Expressions;
 
 namespace ERP_System_Project.Repository.Interfaces
 {
     public interface IRepository<T> where T : class
     {
+        #region Retriving
         Task<T?> GetByIdAsync(int id);
+        IQueryable<T> GetAllToIQueryable();
         Task<List<T>> GetAllAsync();
+
+
+
+        Task<T?> GetAsync(
+            Expression<Func<T, bool>> filter,
+            params Expression<Func<T, object>>[] Includes
+            );
+
+        Task<List<T>> GetAllAsync(
+            Expression<Func<T, bool>> filter,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy,
+            params Expression<Func<T, object>>[] Includes
+            );
+        Task<PageSourcePagination<T>> GetAllPaginatedAsync(
+            int pageNumber = 1, int pageSize = 10,
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            params Expression<Func<T, object>>[] Includes
+            );
+        #endregion
+
+        #region Add_Update_Delete
         Task AddAsync(T entity);
         void Update(T entity);
         void Delete(int id);
+        #endregion
+
+        #region Others
         Task<bool> IsExistAsync(int id);
-
-        Task<List<T>> SkipAndTake(int skip, int take);
-
         Task<int> Count();
-        
+        #endregion
+
     }
 }
