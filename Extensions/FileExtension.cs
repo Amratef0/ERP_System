611e3550096f78868eb667479edc1f2fd5816191
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-09-20 22:40:54 +0300

    change data type from long to int

--- Extensions/FileExtension.cs
+++ Extensions/FileExtension.cs
@@ -2,14 +2,14 @@
 {
     public static class FileExtension
     {
-        public static bool IsValidFile(this IFormFile file, List<string> validExtensions, long maximumSize)
+        public static bool IsValidFile(this IFormFile file, List<string> validExtensions, int maximumSize)
         {
 
             if (file == null || file.Length == 0)
                 throw new ArgumentNullException("File cannot be null or empty.");
 
             if (file.Length > maximumSize)
-                throw new Exception($"File size exceeds the maximum limit of {maximumSize}.");
+                throw new Exception($"File size exceeds the maximum limit of {maximumSize}MB.");
 
             var extension = Path.GetExtension(file.FileName).ToLower();
 
