bf9e245c3de7cc1c8b70689a4a35c7ba2dfde7f2
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-09-20 22:59:22 +0300

    fix logic error

--- Extensions/FileExtension.cs
+++ Extensions/FileExtension.cs
@@ -2,13 +2,13 @@
 {
     public static class FileExtension
     {
-        public static bool IsValidFile(this IFormFile file, List<string> validExtensions, int maximumSize)
+        public static bool IsValidFile(this IFormFile file, List<string> validExtensions, int maximumSize = 1)
         {
 
             if (file == null || file.Length == 0)
                 throw new ArgumentNullException("File Cannot Be Empty");
 
-            if (file.Length > maximumSize)
+            if (file.Length > maximumSize * 1024 * 1024)
                 throw new Exception($"File size exceeds the maximum limit of {maximumSize}MB");
 
             var extension = Path.GetExtension(file.FileName).ToLower();
