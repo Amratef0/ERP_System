e5f9cf8164e5eae2464b1b1adaf2c91129085a77
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-09-20 22:18:38 +0300

    add FileExtension

---
+++ Extensions/FileExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace ERP_System_Project.Extensions
+{
+    public static class FileExtension
+    {
+        public static bool IsValidFile(this IFormFile file, List<string> validExtensions, long maximumSize)
+        {
+
+            if (file == null || file.Length == 0)
+                throw new ArgumentNullException("File cannot be null or empty.");
+
+            if (file.Length > maximumSize)
+                throw new Exception($"File size exceeds the maximum limit of {maximumSize}.");
+
+            var extension = Path.GetExtension(file.FileName).ToLower();
+
+            if(validExtensions.Contains(extension))
+                throw new Exception($"Invalid file extension: {extension}. Expected: {String.Join(", ", validExtensions)}.");
+
+            return true;
+        }
+    }
+}
