bb5995a7b01a4267b2e1e6d0d91ec4f949d0589c
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-09-22 17:11:54 +0300

    Update Erpdbcontext.cs
    Add Purchases DBSET

--- Models/Erpdbcontext.cs
+++ Models/Erpdbcontext.cs
@@ -65,8 +65,16 @@ namespace ERP_System_Project.Models
         public DbSet<InventoryRequisitionItem> InventoryRequisitionItems { get; set; }
         #endregion
 
-
-
+        #region Purchases
+        public DbSet<PaymentTerm> PaymentTerms { get; set; }
+        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
+        public DbSet<PurchaseOrderItem> PurchaseOrderItems { get; set; }
+        public DbSet<PurchaseOrderItemStatusCode> PurchaseOrderItemStatusCodes { get; set; }
+        public DbSet<PurchaseOrderStatusCode> PurchaseOrderStatusCodes { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<SupplierCategory> SupplierCategories { get; set; }
+        public DbSet<SupplierProduct> SupplierProducts { get; set; }
+        #endregion
 
 
         protected override void OnModelCreating(ModelBuilder builder)
