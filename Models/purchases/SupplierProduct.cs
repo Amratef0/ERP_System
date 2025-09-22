15ca1c0b4779b4fd6d746757d073055f576150e9
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-09-22 17:08:31 +0300

    Add purchases Models

---
+++ Models/purchases/SupplierProduct.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ERP_System_Project.Models
+{
+    public class SupplierProduct
+    {
+        [Key]
+        public int SupplierProductId { get; set; }
+
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+ 
+        [StringLength(100)]
+        public string? SupplierProductCode { get; set; }
+
+        [Column(TypeName = "decimal(15,4)")] public decimal UnitPrice { get; set; }
+
+        [Column(TypeName = "decimal(15,4)")] public decimal MinimumOrderQuantity { get; set; } = 0;
+
+        public int? LeadTimeDays { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+    }
+}
