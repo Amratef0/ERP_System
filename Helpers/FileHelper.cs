6bc22dc0f5015c8476a2467a58f4bf632843e960
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-09-30 17:11:41 +0300

    add helper file method to delete old image if user update to new image

---
+++ Helpers/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace ERP_System_Project.Helpers
+{
+    public static class FileHelper
+    {
+        public static async Task DeleteImageFileAsync(string imageUrl)
+        {
+            if (!string.IsNullOrEmpty(imageUrl))
+            {
+                var filePath = Path.Combine("wwwroot", imageUrl.TrimStart('/'));
+                if (File.Exists(filePath))
+                {
+                    await Task.Run(() => File.Delete(filePath));
+                }
+            }
+        }
+    }
+}
