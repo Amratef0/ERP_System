71211a609529ed18fedfa0ec1daced14218917c5
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-10-06 00:13:44 +0300

    add PerformanceLog Controller

---
+++ Controllers/Log/PerformanceLogController.cs
@@ -0,0 +1,18 @@
+﻿using ERP_System_Project.Models.Logs;
+using ERP_System_Project.Services.Interfaces.Log;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_System_Project.Controllers.Log
+{
+    public class PerformanceLogController : Controller
+    {
+        private readonly IPerformanceLogService _performanceLogService;
+
+        public PerformanceLogController(IPerformanceLogService performanceLogService)
+        {
+            _performanceLogService = performanceLogService;
+        }
+        public async Task<IActionResult> Index()
+            => View(await _performanceLogService.GetAllLogsAsync());
+    }
+}
