9344e877e22f6fcd10d625a39aef2acfd4b7e7ed
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-10-06 00:14:05 +0300

    add PerformanceLog Service

---
+++ Services/Implementation/Log/PerformanceLogService.cs
@@ -0,0 +1,23 @@
+﻿using ERP_System_Project.Migrations;
+using ERP_System_Project.Models.Logs;
+using ERP_System_Project.Services.Interfaces.Log;
+using ERP_System_Project.UOW;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP_System_Project.Services.Implementation.Log
+{
+    public class PerformanceLogService : IPerformanceLogService
+    {
+        private readonly IUnitOfWork _uow;
+
+        public PerformanceLogService(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public async Task<List<PerformanceLog>> GetAllLogsAsync()
+            => await _uow.PerformanceLogs
+            .GetAllAsIQueryable()
+            .OrderByDescending(pl => pl.ElabsedTime)
+            .ToListAsync();
+    }
+}
---
+++ Services/Interfaces/Log/IPerformanceLogService.cs
@@ -0,0 +1,9 @@
+﻿using ERP_System_Project.Models.Logs;
+
+namespace ERP_System_Project.Services.Interfaces.Log
+{
+    public interface IPerformanceLogService
+    {
+        Task<List<PerformanceLog>> GetAllLogsAsync();
+    }
+}
