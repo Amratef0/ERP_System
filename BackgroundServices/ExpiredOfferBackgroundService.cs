6a8ea88fc965bb01ac2d934200a91d4098ab64c7
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-10-07 22:08:48 +0300

    Add Background Service Checking Eoired Offers and delete them

---
+++ BackgroundServices/ExpiredOfferBackgroundService.cs
@@ -0,0 +1,53 @@
+﻿using ERP_System_Project.Models.ECommerce;
+using ERP_System_Project.Models.Logs;
+using ERP_System_Project.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP_System_Project.BackgroundServices
+{
+    public class ExpiredOfferBackgroundService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<ExpiredOfferBackgroundService> _logger;
+
+        public ExpiredOfferBackgroundService(IServiceProvider serviceProvider, ILogger<ExpiredOfferBackgroundService> logger)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var now = DateTime.UtcNow;
+                var nextRun = now.Date.AddDays(1); 
+                var delay = nextRun - now;
+
+                try
+                {
+                    // Wait until 12 AM UTC
+                    await Task.Delay(delay, stoppingToken);
+
+                    using (var scope = _serviceProvider.CreateScope())
+                    {
+                        var _repository = scope.ServiceProvider.GetRequiredService<IRepository<Offer>>();
+
+                        await _repository
+                            .GetAllAsIQueryable()
+                            .Where(o => o.EndDate <= DateTime.Now)
+                            .ExecuteDeleteAsync(stoppingToken);
+                    }
+                }
+                catch (TaskCanceledException)
+                {
+                    _logger.LogInformation("Expired Offer BackgroundService stopping gracefully.");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error occurred while Checking offers expired date");
+                }
+            }
+        }
+    }
+}
