a4f5387508927cd002ed086e64d582865dd42059
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-10-09 14:15:08 +0300

    Update InventoryTransaction.cs

--- Models/Inventory/InventoryTransaction.cs
+++ Models/Inventory/InventoryTransaction.cs
@@ -1,7 +1,3 @@
-﻿using ERP_System_Project.Models.HR;
-using ERP_System_Project.Models.ValidationAttributes;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,46 +5,23 @@ namespace ERP_System_Project.Models.Inventory
 {
     public class InventoryTransaction
     {
-        [Key] 
+        [Key]
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Transaction Date is required")]
-        public DateTime TransactionDate { get; set; }
-
-        [DecimalPrecisionScale(19,4)]
-        [Required(ErrorMessage = "Quantity is required")]
-        public decimal Quantity { get; set; }
-
-        [DecimalPrecisionScale(19, 4)]
-        public decimal? UnitCost { get; set; }
-
-        [DecimalPrecisionScale(19, 4)]
-        public decimal? TotalCost { get; set; }
-
-        [StringLength(50, ErrorMessage = "Reference Type Must Be Less Than 50 Characters")]
-        public string? ReferenceType { get; set; }
-        public int? ReferenceId { get; set; }
-
-        [StringLength(1000, ErrorMessage = "Reference Type Must Be Less Than 1000 Characters")]
-        public string? Comments { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
 
         [ForeignKey("Product")]
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product Product { get; set; } = null!;
 
+        [ForeignKey("WarehouseProduct")]
+        public int WarehouseProductId { get; set; }
+        public WarehouseProduct WarehouseProduct { get; set; } = null!;
 
-        [ForeignKey("Warehouse")]
-        public int WarehouseId { get; set; }
-        public Warehouse Warehouse { get; set; }
+        public decimal Quantity { get; set; }
 
-        [ForeignKey("TransactionType")]
-        public int TransactionTypeId { get; set; }
-        public InventoryTransactionType TransactionType { get; set; }
+        public string TransactionType { get; set; } = "Purchase"; // ممكن يكون "Sale" كمان مستقبلاً
 
-        [ForeignKey("Employee")]
-        public int CreatedBy { get; set; }
-        public Employee Employee { get; set; }
+        public DateTime TransactionDate { get; set; } = DateTime.Now;
 
+        public string? Notes { get; set; }
     }
 }
