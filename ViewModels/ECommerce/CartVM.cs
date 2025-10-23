cac55f340c979c2e0d6ff404ecb600b3dd3bc78b
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-10-24 01:50:11 +0300

    add cart view models

---
+++ ViewModels/ECommerce/CartVM.cs
@@ -0,0 +1,8 @@
+﻿namespace ERP_System_Project.ViewModels.ECommerce
+{
+    public class CartVM
+    {
+        public decimal TotalPrice { get; set; }
+        public List<ProductCartVM> productsCart { get; set; } = new List<ProductCartVM>();
+    }
+}
---
+++ ViewModels/ECommerce/ProductCartVM.cs
@@ -0,0 +1,16 @@
+﻿namespace ERP_System_Project.ViewModels.ECommerce
+{
+    public class ProductCartVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ImageURL { get; set; } = null!;
+
+        public decimal Price { get; set; }
+        public bool HaveOffer { get; set; } = false;
+        public int DiscountPercentage { get; set; } = 0;
+        public decimal NetPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
