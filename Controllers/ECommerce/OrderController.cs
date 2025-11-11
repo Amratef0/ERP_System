dd2dee81df3c98edbda607f6ce52f97326e24450
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-11-11 17:22:40 +0200

    add order controller

---
+++ Controllers/ECommerce/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using ERP_System_Project.Services.Interfaces.ECommerce;
+using ERP_System_Project.ViewModels.Inventory;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ERP_System_Project.Controllers.ECommerce
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public async Task<IActionResult> MakeOrder()
+        {
+            await _orderService.MakeOrderAsync();
+            TempData["SuccessMessage"] = "Your order is on its way and will reach you soon";
+            return RedirectToAction("Index","Market");
+        }
+    }
+}
