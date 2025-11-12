b2648143c0c000224ff0b6ff351cc3e73f3336f3
OsamaAhmedX04 <osama.smsma999@gmail.com>
2025-11-12 07:41:03 +0200

    add VMs Related to customer orders

---
+++ ViewModels/ECommerce/MyOrderItemsVM.cs
@@ -0,0 +1,13 @@
+﻿namespace ERP_System_Project.ViewModels.ECommerce
+{
+    public class MyOrderItemsVM
+    {
+        public string ProductName { get; set; }
+        public string ProductImagePath { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountPercentage { get; set; } = 0;
+        public decimal DiscountAmount { get; set; } = 0;
+        public decimal LineTotal { get; set; }
+    }
+}
---
+++ ViewModels/ECommerce/MyOrdersVM.cs
@@ -0,0 +1,24 @@
+﻿using ERP_System_Project.Models.CRM;
+using ERP_System_Project.Models.ECommerce;
+using ERP_System_Project.Models.Inventory;
+using ERP_System_Project.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERP_System_Project.ViewModels.ECommerce
+{
+    public class MyOrdersVM
+    {
+        public string OrderStatus { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal ShippingAmount { get; set; }
+        public string BillingAddress { get; set; }
+        public string PaymentMethodType { get; set; }
+        public DateTime? EstimatedDeliveryDate { get; set; }
+        public DateTime OrderDate { get; set; }
+
+
+        public List<MyOrderItemsVM> OrderItemsVMs { get; set; } = new List<MyOrderItemsVM>();
+        
+    }
+}
