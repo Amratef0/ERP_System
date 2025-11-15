71844e5ae67a4af594d4f01edd5edb47be8e0bf0
zienab-amr <zienabamr2372005@gmail.com>
2025-11-15 19:21:33 +0200

    Revert "Test to deleted"
    This reverts commit af53f14dff147e909e8c248cb68bc5b9d7fabc37.

--- Controllers/Inventory/AttributeController.cs
+++ Controllers/Inventory/AttributeController.cs
@@ -28,7 +28,7 @@ namespace ERP_System_Project.Controllers.Inventory
 
         public async Task<IActionResult> New()
         {
-            return PartialView();
+            return View();
         }
 
         [HttpPost]
@@ -54,7 +54,7 @@ namespace ERP_System_Project.Controllers.Inventory
             if (attribute is null) return NotFound();
             var attributeVM = _mapper.Map<AttributeVM>(attribute);
 
-            return PartialView(attributeVM);
+            return View(attributeVM);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -80,7 +80,7 @@ namespace ERP_System_Project.Controllers.Inventory
             var attribute = await _attributeService.GetByIdAsync(id);
             if (attribute is null) return NotFound();
             var attributeVM = _mapper.Map<AttributeVM>(attribute);
-            return PartialView(attributeVM);
+            return View(attributeVM);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
