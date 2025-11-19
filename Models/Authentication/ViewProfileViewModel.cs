0a733c397acbb42b02930f7820efffc20abc7d90
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-11-19 21:27:44 +0200

    Add files via upload

---
+++ Models/Authentication/ViewProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ERP_System_Project.Models.Authentication
+{
+    public class ViewProfileViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
