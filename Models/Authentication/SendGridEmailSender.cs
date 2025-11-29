a5fbd672f2c45185d9e1e8b7dc2b050bb761590f
Amr Atef <143823614+Amratef0@users.noreply.github.com>
2025-11-29 19:38:07 +0200

    Add files via upload

---
+++ Models/Authentication/SendGridEmailSender.cs
@@ -0,0 +1,24 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.Extensions.Options;
+using ERP_System_Project.Models.Authentication;
+using ERP_System_Project.Services.Interfaces;
+
+public class SendGridEmailSender : IEmailService
+{
+    private readonly SendGridSettings _settings;
+
+    public SendGridEmailSender(IOptions<SendGridSettings> settings)
+    {
+        _settings = settings.Value;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        var client = new SendGridClient(_settings.ApiKey);
+        var from = new EmailAddress(_settings.SenderEmail, _settings.SenderName);
+        var to = new EmailAddress(email);
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+        var response = await client.SendEmailAsync(msg);
+    }
+}
---
+++ Models/Authentication/SendGridSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ERP_System_Project.Models.Authentication
+{
+    public class SendGridSettings
+    {
+        public string ApiKey { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+    }
+
+}
